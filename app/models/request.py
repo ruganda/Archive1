ed2436d2f0e4580bdb8d691a1a01060fd1a64c45
ruganda <mubaruganda@gmail.com>
2018-05-30 06:05:46 +0300

    modify the __nit__ method

--- app/models/request.py
+++ app/models/request.py
@@ -2,8 +2,6 @@ import uuid
 class Request(object):
     """ A class to handle actions related to requests"""
     
-    request_list =[]
-    def __init__(self,item =None,category=None, type=None,description =None):
-        self.item = item
-        self.type =type
-        self.description = description
+    
+    def __init__(self):
+        self.request_list =[]
