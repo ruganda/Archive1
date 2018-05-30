0e24e62afb99f865668734853b082558d88ce00f
ruganda <mubaruganda@gmail.com>
2018-05-30 06:00:53 +0300

    Define and setup the testing class

---
+++ tests/test_user.py
@@ -0,0 +1,7 @@
+"""This module defines tests for the user class and its methods"""
+import unittest
+from app.models.user import User
+
+
+class UserTests(unittest.TestCase):
+    """Define and setup testing class"""
