aae356ec1c97fd6c62e1ff669d28755698195b3c
ruganda <mubaruganda@gmail.com>
2018-05-30 05:54:03 +0300

    Change a class name

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,6 @@
 """This module defines a user class and methods associated to it"""
 
-class User_details(object):
+class User(object):
     """ A class to handle activities related to a user"""
     def __init__(self):
         # A list to hold all user objects
