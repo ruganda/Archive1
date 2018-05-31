1f743fc3e025e8a51d61d02498bd2e6143e68042
ruganda <mubaruganda@gmail.com>
2018-05-31 12:25:51 +0300

    Add a  method to modify a request

--- app/models/request.py
+++ app/models/request.py
@@ -6,22 +6,25 @@ class Request(object):
     def __init__(self):
         self.requests_list =[]
     
-    def create_event(self, id, item, type, category, description):
+    def create_request(self, request_id, item, request_type, category, description):
         """A method to create a new event """
 
         data = {}
         for request_object in self.requests_list :
-            if  id == request_object["id"]:
+            if  request_id == request_object["id"]:
                 return "Request already exists"
-            if id !=request_object["id"]:
+            if request_id !=request_object["id"]:
                 return "request not found"
         else:
-            data["id"] = id
+            data["request_id"] = request_id
             data['item'] = item
             data['category'] = category
-            data['type'] = type
+            data['type'] = request_type
             data['description'] = description
             self.requests_list .append(data)
             return "Request created"
         return "Request does not exist"
+    
+    def modify_test(self):
+        pass
            
