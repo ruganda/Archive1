9cfb1b36b267b451ba993a60ff3e9cabadd9cbc9
ruganda <mubaruganda@gmail.com>
2018-05-31 12:00:15 +0300

    Add a method to create a request

--- app/models/request.py
+++ app/models/request.py
@@ -4,4 +4,24 @@ class Request(object):
     
     
     def __init__(self):
-        self.request_list =[]
+        self.requests_list =[]
+    
+    def create_event(self, id, item, type, category, description):
+        """A method to create a new event """
+
+        data = {}
+        for request_object in self.requests_list :
+            if  id == request_object["id"]:
+                return "Request already exists"
+            if id !=request_object["id"]:
+                return "request not found"
+        else:
+            data["id"] = id
+            data['item'] = item
+            data['category'] = category
+            data['type'] = type
+            data['description'] = description
+            self.requests_list .append(data)
+            return "Request created"
+        return "Request does not exist"
+           
