e4867c121f0c30c59e8f002cf6326704301ff7ba
ruganda <mubaruganda@gmail.com>
2018-06-02 18:56:10 +0300

    Refactor to obey linting rules

--- app/auth/views.py
+++ app/auth/views.py
@@ -8,24 +8,24 @@ from ..import user_instance
 @auth.route('/register/', methods=['POST'])
 def register():
     """A route to handle user"""
-    if request.method == 'POST':
-        data = request.get_json()
-        name = data["name"]
-        username = data['username']
-        password = data['password']
-        # pass the details to the register method
-        try:
-            result = user_instance.register(name,
-                                            username, password)
-            if result == "Registration successfull":
-                return jsonify(response=result), 201
-            else:
-                return jsonify(response=result), 409
-        except Exception as e:
-            response = {
-                'message': str(e)
-            }
-            return jsonify(response), 500
+
+    data = request.get_json()
+    name = data["name"]
+    username = data['username']
+    password = data['password']
+    # pass the details to the register method
+    try:
+        result = user_instance.register(name,
+                                        username, password)
+        if result == "Registration successfull":
+            return jsonify(response=result), 201
+        else:
+            return jsonify(response=result), 409
+    except Exception as e:
+        response = {
+            'message': str(e)
+        }
+        return jsonify(response), 500
 
 
 @auth.route("/login/", methods=["POST"])
