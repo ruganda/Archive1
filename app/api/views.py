1f64dcdfdce3110145d988e84fe00bb71c1e997c
ruganda <mubaruganda@gmail.com>
2018-06-03 13:48:21 +0300

    Secure all the enpoints to require a token

--- app/api/views.py
+++ app/api/views.py
@@ -1,12 +1,43 @@
 from flask import jsonify, request, session, make_response
 from . import api
 from ..import request_instance
+import jwt
 import uuid
+from functools import wraps
+from ..import user_instance
+from ..models.user import User
 
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return jsonify({'message' : 'Token is missing!'}), 401
+
+        try: 
+            data = jwt.decode(token, 'hard to guess string')
+            print(data['username'])
+            for user in user_instance.users_list:
+                if user.get("username"):
+                    current_user = user['username']= data['username']
+                    
+        except:
+            return jsonify({'message' : 'Token is invalid!'}), 401
+
+        return f(current_user, *args, **kwargs)
+
+    return decorated
 
 @api.route('/Request/', methods=['POST', "GET"])
-def create_request():
+@token_required
+def create_request(current_user):
     """A route to handle Requests"""
+   
     if request.method == 'POST':
         request_data = request.get_json()
         request_type = request_data['request_type']
@@ -29,7 +60,8 @@ def create_request():
 
 
 @api.route('/Request/<request_id>', methods=['GET'])
-def fetch_one_request(request_id):
+@token_required
+def fetch_one_request(current_user, request_id):
     """A route to handle request updates"""
     request_id = uuid.UUID(request_id)
     request_object = request_instance.fetch_request_by_id(request_id)
@@ -37,7 +69,8 @@ def fetch_one_request(request_id):
 
 
 @api.route('/Request/<request_id>', methods=['PUT'])
-def modify_request(request_id):
+@token_required
+def modify_request(current_user,request_id):
     """A route to handle requests modification"""
     request_id = uuid.UUID(request_id)
     request_data = request.get_json()
@@ -46,7 +79,8 @@ def modify_request(request_id):
     request_type = request_data['request_type']
     status = request_data['status']
     result = request_instance.modify_request(
-        request_id, item, request_type, description, status)
+                                    request_id, item, request_type,
+                                     description, status)
     if result == "update success":
         return jsonify(response=result), 200
     elif result == "no request with given id":
